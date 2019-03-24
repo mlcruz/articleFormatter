a478c0fadff6ccb4b6d347295c4aec59d859de5a
Matheus Cruz <mlcruz@inf.ufrgs.br>
2019-03-24 14:13:37 -0300

    even more normalization

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import commander = require("commander");
 import { mainPipeline } from "./mainPipeline";
 
-const bib = "bib1/bib/biblio.bib";
-const tex = "bib1/bib/art.tex";
+const bib = "bib1/bib/referencias.bib";
+const tex = "bib1/bib/template.tex";
 
 mainPipeline(bib, tex);
