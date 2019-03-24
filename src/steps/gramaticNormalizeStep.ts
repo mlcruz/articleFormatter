7bdfbcb463c86afa1e3ce97ebff7aa476b44c447
Matheus Cruz <mlcruz@inf.ufrgs.br>
2019-03-24 12:54:32 -0300

    latexAccentNormalization

---
+++ src/steps/gramaticNormalizeStep.ts
@@ -0,0 +1,24 @@
+function normalizeTexAccent(s: string) {
+  const reg = /\{\\'{(\w)/g;
+  const matches = reg.exec(s);
+  if (matches) {
+    s = s.replace(/\{\\'{(\w)}}/, m => {
+      switch (m[4]) {
+        case "a":
+          return "á";
+        case "e":
+          return "é";
+        case "i":
+          return "í";
+        case "o":
+          return "ó";
+        case "u":
+          return "ú";
+        default:
+          return m[4];
+      }
+    });
+    s = normalizeTexAccent(s);
+  }
+  return s;
+}
